c7e9e9f4ab70ff4458f316a8bdb9aee25fb43159
MateoMor <mateomorales5558@gmail.com>
2024-07-14 23:59:11 -0500

    Guards implemented for all endpoints

--- src/projects/controllers/projects.controller.ts
+++ src/projects/controllers/projects.controller.ts
@@ -7,13 +7,22 @@ import {
   ParseUUIDPipe,
   Post,
   Put,
+  UseGuards,
 } from '@nestjs/common';
+import { AccessLevel } from 'src/auth/decorators/access-level.decorator';
+import { AdminAccess } from 'src/auth/decorators/admin.decorator';
+import { AccessLevelGuard } from 'src/auth/guards/access-level.guard';
+import { AuthGuard } from 'src/auth/guards/auth.guard';
+import { RolesGuard } from 'src/auth/guards/roles.guard';
 import { ProjectDTO, ProjectUpdateDTO } from '../dto/projects.dto';
 import { ProjectsService } from '../services/projects.service';
 
 @Controller('projects')
+@UseGuards(AuthGuard, RolesGuard, AccessLevelGuard)
 export class ProjectsController {
   constructor(private readonly projectService: ProjectsService) {}
+
+  @AdminAccess()
   @Post('create')
   public async createProject(@Body() body: ProjectDTO) {
     return await this.projectService.createProject(body);
@@ -24,21 +33,27 @@ export class ProjectsController {
     return await this.projectService.findProjects();
   }
 
-  @Get(':id')
-  public async findProjectById(@Param('id', new ParseUUIDPipe()) id: string) {
+  @Get(':projectId')
+  public async findProjectById(
+    @Param('projectId', new ParseUUIDPipe()) id: string,
+  ) {
     return await this.projectService.findProjectById(id);
   }
 
-  @Put('edit/:id')
+  @AccessLevel('OWNER')
+  @Put('edit/:projectId')
   public async updateProject(
-    @Param('id', new ParseUUIDPipe()) id: string,
+    @Param('projectId', new ParseUUIDPipe()) id: string,
     @Body() body: ProjectUpdateDTO,
   ) {
     return await this.projectService.updateProject(body, id);
   }
 
-  @Delete('delete/:id')
-  public async deleteProject(@Param('id', new ParseUUIDPipe()) id: string) {
+  @AccessLevel('OWNER')
+  @Delete('delete/:projectId')
+  public async deleteProject(
+    @Param('projectId', new ParseUUIDPipe()) id: string,
+  ) {
     return await this.projectService.deleteProject(id);
   }
 }
